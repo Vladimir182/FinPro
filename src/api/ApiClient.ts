cdcb9ff7e9aba8a121645f601cd209d9e284c879
Vitaly Baryliuk <v.baryliuk@devcaz.com>
2020-09-04 14:34:46 +0300

    Fixing cors error at develop server

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -97,9 +97,9 @@ export default class ApiClient {
 		const res = Axios({
 			// ${process.env.REACT_APP_URL}
 			method: 'POST',
-			url: `/${this.prefix}${url}${query}`,
+			url: `${process.env.REACT_APP_URL}/${this.prefix}${url}${query}`,
 			data: method !== 'GET' ? body : null,
-			withCredentials: false,
+			withCredentials: true,
 		});
 
 		return this.handleResponse(res);
