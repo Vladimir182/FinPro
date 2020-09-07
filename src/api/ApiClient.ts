fa3cdddfde08da9236950d8d920116023c6e3c58
Vitaly Baryliuk <v.baryliuk@devcaz.com>
2020-09-07 13:50:29 +0300

    changed api link to 1

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -98,7 +98,6 @@ export default class ApiClient {
 
 		const res = Axios({
 			// ${process.env.REACT_APP_URL}
-			//${process.env.REACT_APP_CORS_ANYWHERE}/
 			method: 'POST',
 			url: `${process.env.REACT_APP_URL2}/${this.prefix}${url}${query}`,
 			data: method !== 'GET' ? body : null,
