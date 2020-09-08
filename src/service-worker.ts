9d19fc45bc901127874840e52dee6f6660c6e5f9
Vitaly Baryliuk <v.baryliuk@devcaz.com>
2020-09-08 17:18:14 +0300

    added static to service worker url

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -29,7 +29,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `./service-worker.ts`;
+      const swUrl = `./static/js/service-worker.ts`;
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
