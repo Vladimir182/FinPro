64990ec817380a4fad84b5ee4797f0298ce8a6c5
Vitaly Baryliuk <v.baryliuk@devcaz.com>
2020-09-08 16:58:23 +0300

    changed finpro.js to finpro.ts

--- src/finproSW.ts
+++ src/finproSW.ts
@@ -29,7 +29,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `./finproSW.js`;
+      const swUrl = `./finproSW.ts`;
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
