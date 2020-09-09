0f94af633fbc04231f28a6831d0580ce7adcfedb
Vitaly Baryliuk <v.baryliuk@devcaz.com>
2020-09-09 15:45:34 +0300

    fixed path to sw

--- src/sw.ts
+++ src/sw.ts
@@ -64,7 +64,7 @@ export function register(config?: Config) {
 
     window.addEventListener('load', () => {
       // const swUrl = `./static/js/service-worker.js`;
-      const swUrl = `./sw.js`;
+      const swUrl = `./static/js/sw.js`;
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
