1646a77b28886a3e0f224dc64c3002324bb270f2
Vitaly Baryliuk <v.baryliuk@devcaz.com>
2020-11-30 12:06:57 +0200

    Implemented urls for prod api and ws server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use('/', createProxyMiddleware(['/cashpro', '/oauth2'],{
-    target: process.env.REACT_APP_URL_STAGE,
+    target: process.env.REACT_APP_URL_PROD,
     changeOrigin: true,
     secure:false
   }));
   app.use('/', createProxyMiddleware('/socket', { 
-    target: process.env.REACT_APP_WS_URL_STAGE,
+    target: process.env.REACT_APP_WS_URL_PROD,
     changeOrigin: true,
     secure: false,
     ws: true 
