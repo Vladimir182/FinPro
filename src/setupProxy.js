ff8845ec51e0877d0ca7b649bdbbfeb78dfad3a5
Vitaly Baryliuk <v.baryliuk@devcaz.com>
2020-11-30 11:32:30 +0200

    Implemented stage urls for proxying

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use('/', createProxyMiddleware(['/cashpro', '/oauth2'],{
-    target: process.env.REACT_APP_URL2,
+    target: process.env.REACT_APP_URL_STAGE,
     changeOrigin: true,
     secure:false
   }));
   app.use('/', createProxyMiddleware('/socket', { 
-    target: process.env.REACT_APP_WS_URL,
+    target: process.env.REACT_APP_WS_URL_STAGE,
     changeOrigin: true,
     secure: false,
     ws: true 
